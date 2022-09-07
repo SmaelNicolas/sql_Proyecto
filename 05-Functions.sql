4b40f070aa31d8c4b523e2ca1318b093973f2fd2
Smael Nicolas <nico.smael@gmail.com>
2022-09-07 16:30:15 -0300

    new functions get full name / ids / names / adress

--- 05-Functions.sql
+++ 05-Functions.sql
@@ -1,7 +1,8 @@
 use 1928_showroom;
 
-#Devuelve el id del cliente segun numero de documento 
 delimiter / / 
+
+#Devuelve el id del cliente segun numero de documento 
 create function get_client_id  (DNI_entry int) 
 	returns int 
     DETERMINISTIC 
@@ -11,10 +12,8 @@ create function get_client_id  (DNI_entry int)
 		return id_client_return;
 
 END ;
-//delimiter;
 
 #Devuelve el DNI del cliente segun Id 
-delimiter / / 
 create function get_client_dni  (id_entry int) 
 	returns int 
     DETERMINISTIC 
@@ -24,10 +23,8 @@ create function get_client_dni  (id_entry int)
 		return dni_client_return;
 
 END ;
-//delimiter;
 
 #Devuelve el nombre completo del cliente segun DNI 
-delimiter / / 
 create function get_client_full_name  (DNI_entry int) 
 	returns varchar(50) 
     DETERMINISTIC 
@@ -43,10 +40,8 @@ create function get_client_full_name  (DNI_entry int)
 		return (full_name_client_return);
 
 END ;
-//delimiter;
 
 #Devuelve la direccion completa del cliente segun DNI 
-delimiter / / 
 create function get_client_full_adress  (DNI_entry int) 
 	returns varchar(100) 
     DETERMINISTIC 
@@ -64,10 +59,8 @@ create function get_client_full_adress  (DNI_entry int)
 		return (full_adress_client_return);
 
 END ;
-//delimiter;
 
 #devuelve el precio del producto segun id
-delimiter / / 
 create function get_product_price  (id_entry int) 
 	returns float(2) 
     DETERMINISTIC 
@@ -77,10 +70,8 @@ create function get_product_price  (id_entry int)
 		return price_return;
 
 END ;
-//delimiter;
 
 #devuelve el stock del producto segun id
-delimiter / / 
 create function get_product_stock  (id_entry int) 
 	returns float(2) 
     DETERMINISTIC 
@@ -90,11 +81,8 @@ create function get_product_stock  (id_entry int)
 		return stock_return;
 
 END ;
-//delimiter;
-
 
 #Devuelve el id del talle del producto ingresando el talle
-delimiter / / 
 create function get_size_id  (size_entry char(25)) 
 	returns int 
     DETERMINISTIC 
@@ -104,10 +92,8 @@ create function get_size_id  (size_entry char(25))
 		return size_return;
 
 END ;
-//delimiter;
 
 #Devuelve el nombre del talle del producto ingresando el id
-delimiter / / 
 create function get_size_name  (id_entry int) 
 	returns char(25) 
     DETERMINISTIC 
@@ -117,10 +103,8 @@ create function get_size_name  (id_entry int)
 		return name_return;
 
 END ;
-//delimiter;
 
 #Devuelve el id del tipo del producto ingrensado el tipo
-delimiter / / 
 create function get_type_id  (type_entry int) 
 	returns int 
     DETERMINISTIC 
@@ -129,10 +113,8 @@ create function get_type_id  (type_entry int)
         set type_return = (select id_type from product_types where type_entry = name_type  );
 		return type_return;
 END ;
-//delimiter;
 
 #Devuelve el nombre del tipo del producto ingrensado el id
-delimiter / / 
 create function get_type_name  (id_entry int) 
 	returns char(25) 
     DETERMINISTIC 
@@ -141,10 +123,8 @@ create function get_type_name  (id_entry int)
         set name_return = (select name_type from product_types where id_entry = id_type );
 		return name_return;
 END ;
-//delimiter;
 
 #Devuelve el id ingresando la cantidad de puntos
-delimiter / / 
 create function get_points_id  (points_entry int) 
 	returns int 
     DETERMINISTIC 
@@ -153,10 +133,8 @@ create function get_points_id  (points_entry int)
         set point_return = (select id_points from product_points where points_entry = amount_points  );
 		return point_return;
 END ;
-//delimiter;
 
 #Devuelve la cantidad de puntos ingresando el id
-delimiter / / 
 create function get_points_amount  (id_entry int) 
 	returns int 
     DETERMINISTIC 
@@ -165,10 +143,8 @@ create function get_points_amount  (id_entry int)
         set amount_return = (select amount_points from product_points where id_entry = id_points );
 		return amount_return;
 END ;
-//delimiter;
 
 #Devuelve el id ingresando la cantidad de descuento
-delimiter / / 
 create function get_discount_id  (discount_entry int) 
 	returns int 
     DETERMINISTIC 
@@ -177,10 +153,8 @@ create function get_discount_id  (discount_entry int)
         set id_return = (select id_discount from discount where discount_entry = total_discount  );
 		return id_return;
 END ;
-//delimiter;
 
 #Devuelve la cantidad de descuento ingresando el id
-delimiter / / 
 create function get_discount_amount  (id_entry int) 
 	returns int 
     DETERMINISTIC 
@@ -189,10 +163,8 @@ create function get_discount_amount  (id_entry int)
         set discount_return = (select total_discount from discount where id_entry = id_discount );
 		return discount_return;
 END ;
-//delimiter;
 
 #Devuelve el id ingresando el beneficio
-delimiter / / 
 create function get_benefit_id  (benefit_entry char(50)) 
 	returns int 
     DETERMINISTIC 
@@ -201,12 +173,8 @@ create function get_benefit_id  (benefit_entry char(50))
         set id_return = (select id_benefit from benefits where benefit_entry = name_benefit  );
 		return id_return;
 END ;
-//delimiter;
-
-select * from benefits;
 
 #Devuelve el beneficio ingresando el id
-delimiter / / 
 create function get_benefit_name  (id_entry int) 
 	returns varchar(50) 
     DETERMINISTIC 
@@ -215,10 +183,8 @@ create function get_benefit_name  (id_entry int)
         set name_return = (select name_benefit from benefits where id_entry = id_benefit );
 		return name_return;
 END ;
-//delimiter;
 
 #Devuelve los puntos de costo de uso ingresando el id del beneficio
-delimiter / / 
 create function get_benefit_cost  (id_entry int) 
 	returns int 
     DETERMINISTIC 
@@ -227,4 +193,77 @@ create function get_benefit_cost  (id_entry int)
         set points_return = (select points_benefit from benefits where id_entry = id_benefit );
 		return points_return;
 END ;
+
+#Devuelve el id ingresando el tipo de delivery
+create function get_delivery_method_id  (delivery_method_entry char(50)) 
+	returns int
+    DETERMINISTIC 
+	BEGIN 
+		DECLARE id_return int ;
+        set id_return = (select id_delivery from delivery_method where delivery_method_entry = name_delivery  );
+		return id_return;
+END ;
+
+#Devuelve el tipo de delivery ingresando el id
+create function get_delivery_method_name  (id_entry int) 
+	returns varchar(50) 
+    DETERMINISTIC 
+	BEGIN 
+		DECLARE name_return varchar(50);
+        set name_return = (select name_delivery from delivery_method where id_entry = id_delivery );
+		return name_return;
+END ;
+
+#Devuelve el costo del delivery ingresando el id
+delimiter / / 
+create function get_delivery_method_cost  (id_entry int) 
+	returns int 
+    DETERMINISTIC 
+	BEGIN 
+		DECLARE cost_return int;
+        set cost_return = (select price_delivery from delivery_method where id_entry = id_delivery );
+		return cost_return;
+END ;
+
+#Devuelve el id ingresando el estado de orden
+create function get_order_state_id  (order_state_entry char(50)) 
+	returns int
+    DETERMINISTIC 
+	BEGIN 
+		DECLARE id_return int ;
+        set id_return = (select id_state from order_state where order_state_entry = name_state  );
+		return id_return;
+END ;
+
+#Devuelve el estado de orden ingresando el id
+create function get_order_state_name  (id_entry int) 
+	returns varchar(50) 
+    DETERMINISTIC 
+	BEGIN 
+		DECLARE name_return varchar(50);
+        set name_return = (select name_state from order_state where id_entry = id_state );
+		return name_return;
+END ;
+
+#Devuelve el id ingresando la forma de pago
+create function get_payment_method_id  (payment_method_entry char(50)) 
+	returns int
+    DETERMINISTIC 
+	BEGIN 
+		DECLARE id_return int ;
+        set id_return = (select id_method from payment_method where payment_method_entry = name_method  );
+		return id_return;
+END ;
+
+#Devuelve la forma de pago ingresando el id
+create function get_payment_method_name  (id_entry int) 
+	returns varchar(50) 
+    DETERMINISTIC 
+	BEGIN 
+		DECLARE name_return varchar(50);
+        set name_return = (select name_method from payment_method where id_entry = id_method );
+		return name_return;
+END ;
+
+
 //delimiter;
